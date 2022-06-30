8289c55b7d414fd33a6954d839b3a6aaaa73f5a3
agileMeadow <107934819+agileMeadow@users.noreply.github.com>
2022-06-30 09:36:14 +0900

    Update MemberInput.java

--- MemberInput.java
+++ MemberInput.java
@@ -2,42 +2,42 @@ package p2022_06_29;
 
 import java.util.Scanner;
 
-public class MemberInput {
+public class Github0629 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Your Name: ");
-		Scanner sc1 = new Scanner(System.in);
-		String name1 = sc1.nextLine();
+		Scanner sc = new Scanner(System.in);
+		String name1 = sc.nextLine();
 
 		System.out.println("Your Age: ");
-		Scanner sc2 = new Scanner(System.in);
-		int age1 = sc2.nextInt();
+		int age1 = sc.nextInt();
+		sc.nextLine();
 
 		System.out.println("Your Email: ");
-		Scanner sc3 = new Scanner(System.in);
-		String email1 = sc3.nextLine();
+		String email1 = sc.nextLine();
 
 		System.out.println("Your Address: ");
-		Scanner sc4 = new Scanner(System.in);
-		String address1 = sc4.nextLine();
+		String address1 = sc.nextLine();
 		
-		MemberInfo m = new MemberInfo(name1, age1, email1, address1);
+		MemberInfo2 m = new MemberInfo2(name1, age1, email1, address1);
 		//To call MemberInfo and save the info there.
 		//m is the reference variable
 		m.print();
+		
+		sc.close();
 	}
 		
 }
 
-class MemberInfo {
+class MemberInfo2 {
 	private String name;
 	private int age;
 	private String email;
 	private String address;
-
+	
 	// Class
-	public MemberInfo(String name, int age, String email, String address) {
+	public MemberInfo2(String name, int age, String email, String address) {
 		this.name = name;
 		this.age = age;
 		this.email = email;
